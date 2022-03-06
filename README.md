53552ad2d3c55d05ed87fe5393c3b0369f27f102
William Clot <william.clot@ravelin.com>
2022-03-06 22:17:33 +0000

    healthcheck

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Emby exporter
 
-Prometheus exporter for Emby media server metrics. 
+
+```
+NAME:
+  emby_exporter - A Prometheus exporter that exports metrics on Emby Media Server.
+
+USAGE:
+  emby_exporter [options]
+
+OPTIONS:
+  --telemetry.addr        Port for server (default: ":9162")
+  --telemetry.path        URL path for metric collection (default: "/metrics")
+  --emby.verifyTLS        Verify TLS certificate of Emby Server (default: true)
+  --emby.addr             URL address of Emby API
+  --emby.token            Emby API token
+  --health                Run a healthcheck of the exporter
+  --help, -h              Show help
+```
+
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -13,34 +16,56 @@ import (
 )
 
 var (
-	telemetryAddr = flag.String("telemetry.addr", ":9162", "address for the emby exporter")
+	telemetryAddr = flag.String("telemetry.addr", ":9162", "address for the Emby exporter")
 	metricsPath   = flag.String("telemetry.path", "/metrics", "URL path for surfacing collected metrics")
 
-	embyAddr = flag.String("emby.addr", ":8096", "address of emby API")
-	embyKey  = flag.String("emby.key", "", "emby API key")
+	embyAddr      = flag.String("emby.addr", ":8096", "address of Emby API")
+	embyToken     = flag.String("emby.token", "", "Emby API key")
+	embyVerifyTLS = flag.Bool("emby.verifyTLS", true, "verify TLS certificate of Emby Server")
+
+	healthcheck = flag.Bool("health", false, "runs a small healthcheck against the exporter itself")
 )
 
 func main() {
 	flag.Parse()
-	if *embyAddr == "" {
-		log.Fatal("address of emby server must be specified with '-emby.addr' flag")
+
+	if *healthcheck {
+		c := http.Client{
+			Timeout: time.Second,
+		}
+		req, err := http.NewRequest("GET", fmt.Sprintf("127.0.0.1%s/health", *telemetryAddr), nil)
+		if err != nil {
+			os.Exit(1)
+		}
+		res, err := c.Do(req)
+		if err != nil || res.StatusCode != http.StatusOK {
+			os.Exit(1)
+		}
+		os.Exit(0)
 	}
-	if *embyKey == "" {
-		log.Fatal("API key of emby server must be specified with '-emby.key' flag")
+
+	if *embyAddr == "" {
+		log.Fatal("address of Emby server must be specified with '-emby.addr' flag")
+	}
+	if *embyToken == "" {
+		log.Fatal("API key of Emby server must be specified with '-emby.key' flag")
 	}
 
 	ctx := context.Background()
-	client := emby.New(ctx, *embyAddr, *embyKey)
+	client := emby.New(ctx, *embyAddr, *embyToken, *embyVerifyTLS)
 
 	// Creating prometheus registry and handler
 	r := prometheus.NewRegistry()
 	r.MustRegister(collector.New(client))
 	mux := http.NewServeMux()
 	mux.Handle(*metricsPath, promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+	mux.Handle("/health", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
 
 	// Start listening for HTTP connections.
-	log.Printf("starting emby exporter on %q", *telemetryAddr)
+	log.Printf("starting Emby exporter on %q", *telemetryAddr)
 	if err := http.ListenAndServe(*telemetryAddr, mux); err != nil {
-		log.Fatalf("cannot start emby exporter: %s", err)
+		log.Fatalf("cannot start Emby exporter: %s", err)
 	}
 }
